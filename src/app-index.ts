517e37794815f51f355195cb7bae6429baf0bc0f
Sean Wong <seanwong24@live.com>
2023-01-04 16:20:10 -0800

    remove some margins

--- src/app-index.ts
+++ src/app-index.ts
@@ -11,12 +11,6 @@ import './styles/global.css';
 export class AppIndex extends LitElement {
   static get styles() {
     return css`
-      main {
-        padding-left: 16px;
-        padding-right: 16px;
-        padding-bottom: 16px;
-      }
-
       #routerOutlet > * {
         width: 100% !important;
       }
